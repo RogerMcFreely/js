a0cd295e89849192e698fad6f889d73080864b0e
RogerMcFreely <46225864+RogerMcFreely@users.noreply.github.com>
2018-12-28 11:49:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Code goes here
+var animal = "cat";
+
+switch (animal) {
+  case "cat":
+    alert("meow");
+    break;
+  case "dog":
+    alert("woof");
+    break;
+  case "horse":
+    alert("neigh");
+    break;
+  default:
+    alert("Unknow animal!");
+    break;
+}
